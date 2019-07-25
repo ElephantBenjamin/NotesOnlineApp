b0f6b746e285e1a00f66e93bf9ac229af92159ff
ElephantBenjamin <elephantbenjamin@gmail.com>
2019-07-25 23:43:10 +0500

    Testing UI and taking data from DB Notes display 1st variant

--- src/com/NotesOnlineApp.java
+++ src/com/NotesOnlineApp.java
@@ -1,13 +1,62 @@
 package com;
 
+import com.models.Note;
+import com.services.NoteService;
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.server.VaadinRequest;
+import com.vaadin.shared.ui.ContentMode;
 import com.vaadin.ui.*;
 
 public class NotesOnlineApp extends UI {
+
+    private void initSearchPanel(Layout mainLayout) {
+        Panel searchPanel = new Panel();
+        searchPanel.setStyleName("borderless");
+
+        TextField searchField = new TextField();
+        searchField.setPlaceholder("Search");
+
+        Button searchButton = new Button();
+        searchButton.setIcon(VaadinIcons.SEARCH);
+
+        Button addNoteButton = new Button("New note");
+        addNoteButton.setIcon(VaadinIcons.PLUS);
+
+        HorizontalLayout searchLayout = new HorizontalLayout();
+        searchLayout.addComponent(searchField);
+        searchLayout.addComponent(searchButton);
+        searchLayout.addComponent(addNoteButton);
+        searchPanel.setContent(searchLayout);
+        mainLayout.addComponent(searchPanel);
+    }
+
     @Override
     public void init(VaadinRequest request) {
         VerticalLayout layout = new VerticalLayout();
         setContent(layout);
-        layout.addComponent(new Label("Hello, world!"));
+
+        initSearchPanel(layout);
+
+        NoteService noteService = new NoteService();
+        for (Note note : noteService.findAllNotes()) {
+            HorizontalLayout noteLayout = new HorizontalLayout();
+            Panel notePanel = new Panel();
+            VerticalLayout notePanelLayout = new VerticalLayout();
+            Label titleLabel = new Label("<b>Untitled</b>");
+            if (note.getTitle() != null)
+                titleLabel.setValue("<b>" + note.getTitle() + "</b>");
+            titleLabel.setContentMode(ContentMode.HTML);
+            Label textLabel = new Label(note.getText());
+            notePanelLayout.addComponent(titleLabel);
+            notePanelLayout.addComponent(textLabel);
+            notePanel.setContent(notePanelLayout);
+            noteLayout.addComponent(notePanel);
+
+            Button deleteNoteButton = new Button();
+            deleteNoteButton.setIcon(VaadinIcons.TRASH);
+            noteLayout.addComponent(deleteNoteButton);
+            layout.addComponent(noteLayout);
+        }
+
     }
 }
